1627f0c53db0540d27eb9c7f939ff1aae3f7e246
sitaramaraju123 <sitaramaraju123463@gmail.com>
2021-08-08 08:31:26 +0530

    new Update

--- arguments.js
+++ arguments.js
@@ -1,10 +1,10 @@
-function arg(){
-    let total = 0;
-   // for(let val=1; val<= arguments.length; val++)
-   //                 (or)
-      for(let val of arguments)    //This is for of loop iterator
-    total += val;
-    return total;
+function arg() {
+  let total = 0;
+  // for(let val=1; val<= arguments.length; val++)
+  //                 (or)
+  for (let val of arguments) //This is for of loop iterator
+    total += val + 1;
+  return total;
 }
 
-console.log(arg(1,2,3,4,5,6));
+console.log(arg(1, 2, 3, 4, 5, 6));
